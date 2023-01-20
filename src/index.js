b0dbd6ea750039343a8d70f8ae7e9ac0c0fb6a4d
Yuriy Stetskyy <63265099+kenzaofficial@users.noreply.github.com>
2023-01-20 18:54:15 +0200

    hero section started, added phone mask, mobile version is rdy (#7)
    * hero section started, added phone mask, mobile version is rdy
    
    * button added
    
    * desktop version is rdy
    
    * hero block is ready v2.
    
    * requested changes are fixed
    
    * minor changes
    
    * variables names have been changed, scales deleted,js-form deleted,focus inputs aded
    
    * some colors have been changed
    
    Co-authored-by: henker <henkerofficial@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,36 +1,4 @@
 require('./pages/main/main');
 
-// eslint-disable-next-line no-undef
-const doc = document;
-
-doc.addEventListener('DOMContentLoaded', () => {
-  const headerBurger = doc.querySelector('.header__burger-button');
-  const menuLink = doc.querySelectorAll('.menu__link');
-  const body = doc.querySelector('body');
-  const headerBody = doc.querySelector('.header__body');
-  const overlay = doc.querySelector('.overlay');
-
-  if (body.classList.contains('no-js')) {
-    body.classList.remove('no-js');
-  }
-
-  headerBurger.addEventListener('click', () => {
-    headerBody.classList.toggle('header-menu--is-active');
-    body.classList.toggle('scroll-lock');
-    overlay.classList.toggle('visually-hidden');
-  });
-
-  overlay.addEventListener('click', () => {
-    headerBody.classList.remove('header-menu--is-active');
-    body.classList.remove('scroll-lock');
-    overlay.classList.add('visually-hidden');
-  });
-
-  for (let i = 0; i < menuLink.length; i += 1) {
-    menuLink[i].addEventListener('click', () => {
-      headerBody.classList.remove('header-menu--is-active');
-      body.classList.remove('scroll-lock');
-      overlay.classList.add('visually-hidden');
-    });
-  }
-});
+require('./components/header/header');
+require('./pages/main/sections/hero/phone-mask');
